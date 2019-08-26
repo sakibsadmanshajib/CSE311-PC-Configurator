52f8bcedf784454022ed5e9fe5bddbbe8ab070b0
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2019-08-26 23:02:20 +0600

    models added

--- pcconfig/models.py
+++ pcconfig/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class PC(models.Model):
+    pass
+
+class CPU(models.Model):
+    pass
+
+class RAM(models.Model):
+    pass
+
+class GPU(models.Model):
+    pass
+
+class Motherboard(models.Model):
+    pass
+
+class PSU(models.Model):
+    pass
+
+class Storage(models.Model):
+    pass
